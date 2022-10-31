ca3abf160cf85f6788db04bb4472abd1c88fe68f
andrewelamb <andrewelamb@gmail.com>
2022-10-31 14:38:57 -0700

    add annotate table method

--- schematic_db/rdb/synapse.py
+++ schematic_db/rdb/synapse.py
@@ -1,8 +1,20 @@
 """SynapseDatabase"""
 import pandas as pd
-from schematic_db.db_config import DBObjectConfig
+from schematic_db.db_config import DBObjectConfig, ForeignKey
 from schematic_db.synapse import Synapse, SynapseConfig
 from .rdb import RelationalDatabase
+
+
+def create_foreign_key_annotation_string(key: ForeignKey) -> str:
+    """Creates a string that will serve as a foreign key Synapse annotation
+
+    Args:
+        key (ForeignKey): The foreign key that to be turned into a string
+
+    Returns:
+        str: The foreign key in string form.
+    """
+    return f"{key.name};{key.foreign_object_name};{key.foreign_attribute_name}"
 
 
 class SynapseDatabase(RelationalDatabase):
@@ -32,6 +44,7 @@ class SynapseDatabase(RelationalDatabase):
         # table doesn't exist in Synapse, and must be built
         if table_name not in table_names:
             self.synapse.build_table(table_name, data)
+            self.annotate_table(table_name, table_config)
             return
 
         # table exists but has no columns/rows, both must be added
@@ -39,14 +52,17 @@ class SynapseDatabase(RelationalDatabase):
         if len(list(current_columns)) == 0:
             self.synapse.add_table_columns(table_name, data)
             self.synapse.insert_table_rows(table_name, data)
+            self.annotate_table(table_name, table_config)
             return
 
         # table exists and possibly has data, upsert method must be used
         self.synapse.upsert_table_rows(table_name, data, table_config)
 
     def drop_table(self, table_name: str) -> None:
-        self.synapse.delete_all_table_rows(table_name)
-        self.synapse.delete_all_table_columns(table_name)
+        synapse_id = self.synapse.get_synapse_id_from_table_name(table_name)
+        self.synapse.delete_all_table_rows(synapse_id)
+        self.synapse.delete_all_table_columns(synapse_id)
+        self.synapse.clear_entity_annotations(synapse_id)
 
     def delete_table_rows(
         self, table_name: str, data: pd.DataFrame, table_config: DBObjectConfig
@@ -55,3 +71,20 @@ class SynapseDatabase(RelationalDatabase):
 
     def get_table_names(self) -> list[str]:
         return self.synapse.get_table_names()
+
+    def annotate_table(self, table_name: str, table_config: DBObjectConfig) -> None:
+        """Annotates the table with it's primary key and foreign keys
+
+        Args:
+            table_name (str): The name of the table to be annotated
+            table_config (DBObjectConfig):The config for the table
+        """
+        synapse_id = self.synapse.get_synapse_id_from_table_name(table_name)
+        annotations = {
+            "primary_key": table_config.primary_key,
+            "foreign_keys": [
+                create_foreign_key_annotation_string(key)
+                for key in table_config.foreign_keys
+            ],
+        }
+        self.synapse.set_entity_annotations(synapse_id, annotations)
