96e5601070daec6569f9af4c7de0749330a1199d
andrewelamb <andrewelamb@gmail.com>
2023-03-31 09:01:26 -0700

    converted ManifestSynapseConfig to pydantic

--- schematic_db/api_utils/api_utils.py
+++ schematic_db/api_utils/api_utils.py
@@ -1,7 +1,9 @@
 """Functions that interact with the schematic API"""
 
-from dataclasses import dataclass
+import re
 from os import getenv
+from pydantic.dataclasses import dataclass
+from pydantic import validator
 import requests
 import pandas
 
@@ -109,7 +111,7 @@ def get_graph_by_edge_type(schema_url: str, relationship: str) -> list[tuple[str
     return response.json()
 
 
-@dataclass
+@dataclass()
 class ManifestSynapseConfig:
     """A config for a manifest in Synapse."""
 
@@ -118,6 +120,42 @@ class ManifestSynapseConfig:
     manifest_id: str
     manifest_name: str
     component_name: str
+
+    @validator("dataset_id", "manifest_id")
+    @classmethod
+    def validate_synapse_id(cls, value: str) -> str:
+        """Check if string is a valid synapse id
+
+        Args:
+            value (str): A string
+
+        Raises:
+            ValueError: If the value isn't a valid Synapse id
+
+        Returns:
+            (str): The input value
+        """
+        if not re.search("^syn[0-9]+", value):
+            raise ValueError(f"{value} is not a valid Synapse id")
+        return value
+
+    @validator("dataset_name", "manifest_name", "component_name")
+    @classmethod
+    def validate_string_is_not_empty(cls, value: str) -> str:
+        """Check if string  is nto empty(has at least one char)
+
+        Args:
+            value (str): A string
+
+        Raises:
+            ValueError: If the value is zero characters long
+
+        Returns:
+            (str): The input value
+        """
+        if len(value) == 0:
+            raise ValueError(f"{value} is an empty string")
+        return value
 
 
 def get_project_manifests(
